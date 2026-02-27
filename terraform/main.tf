8376cb19815c883c1d7512dd3f511999dbd1e794
dford-opsec <darrenford@opsec.cloud>
2026-02-27 14:42:03 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,6 +83,18 @@ resource "google_project_iam_member" "vulnerable_sa_compute_admin" {
   member  = "serviceAccount:${google_service_account.vulnerable_sa.email}"
 }
 
+resource "google_project_iam_member" "vulnerable_sa_compute_admin" {
+  project = trimspace(var.project_id)
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.vulnerable_sa.email}"
+}
+
+resource "google_project_iam_member" "vulnerable_sa_compute_admin" {
+  project = trimspace(var.project_id)
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.vulnerable_sa.email}"
+}
+
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
